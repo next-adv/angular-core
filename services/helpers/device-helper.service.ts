25daae35ba197f67feb334087b02363cfae69ea6
Riccardo Coco <coco@next.sa.it>
2020-01-27 13:07:16 +0100

    feat(Helper) added

---
+++ services/helpers/device-helper.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+import {Device} from '@ionic-native/device/ngx';
+
+import Constants from '../../angular-core/Constants';
+
+interface IDevice {
+    regId: string;
+    uuid: string;
+    platform: string;
+    version: string;
+    manufacturer: string;
+    model: string;
+    serial: string;
+    locale: string;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DeviceHelperService {
+
+    constructor(
+        private device: Device
+    ) {}
+
+    getDeviceObject(registrationId?: string): IDevice {
+        return {
+            regId: registrationId,
+            uuid: this.device.uuid,
+            platform: this.device.platform,
+            version: this.device.version,
+            manufacturer: this.device.manufacturer,
+            model: this.device.model,
+            serial: this.device.serial,
+            locale: Constants.locale
+        };
+    }
+}
---
+++ services/helpers/ui-helper.service.ts
@@ -0,0 +1,76 @@
+import {Injectable} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {LoadingController, ToastController} from '@ionic/angular';
+
+@Injectable({
+    prvidedIn: 'root'
+})
+export class UIHelperService {
+
+    public loading: any;
+    public currentViewToDismiss: any;
+
+    constructor(
+        private translate: TranslateService,
+        private loadingController: LoadingController,
+        private toastController: ToastController
+        ) {
+    }
+
+    async showLoader(msg?) {
+        if (!this.loading) {
+            if (!msg) {
+                msg = await this.translate.get('LOADING').toPromise();
+            } else {
+                msg = await this.translate.get(msg).toPromise();
+            }
+
+            this.loading = await this.loadingController.create({
+                message: msg,
+                spinner: 'bubbles'
+            });
+            await this.loading.present();
+            this.loading.onDidDismiss().then(res => {
+                this.currentViewToDismiss = null;
+            });
+            this.currentViewToDismiss = this.loading;
+        }
+    }
+
+    dismissLoader() {
+        this.loadingController.dismiss().then(() => {
+            this.currentViewToDismiss = null;
+            this.loading = null;
+        });
+    }
+
+    async showToast(msg?, color?, position?) {
+        if (!position) {
+            position = 'bottom';
+        }
+        if (!color) {
+            color = 'danger';
+        }
+        if (!msg) {
+            await this.translate.get('GENERIC_ERROR').subscribe(async res => {
+                msg = res;
+                const toast = await this.toastController.create({
+                    message: res,
+                    duration: 2000,
+                    color
+                });
+                await toast.present();
+            });
+        } else {
+            await this.translate.get(msg).subscribe(async res => {
+                const toast = await this.toastController.create({
+                    message: res,
+                    duration: 2000,
+                    position,
+                    color
+                });
+                await toast.present();
+            });
+        }
+    }
+}
