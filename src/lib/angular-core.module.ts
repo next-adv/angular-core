840f22bd73e572a916d0320a20dcd1f9d12b75e3
Riccardo Coco <coco@next.sa.it>
2020-02-18 15:38:52 +0100

    code lint

--- src/lib/angular-core.module.ts
+++ src/lib/angular-core.module.ts
@@ -4,20 +4,13 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {IonicModule} from '@ionic/angular';
 import {TranslateModule} from '@ngx-translate/core';
 import {RouterModule} from '@angular/router';
-//import {Facebook} from '@ionic-native/facebook/ngx';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { IonicStorageModule } from '@ionic/storage';
 
-/*import {SignUpComponent} from './ionic-components/sign-up/sign-up.component';
-import {LoginComponent} from './ionic-components/login/login.component';*/
-import {ButtonSwitchComponent} from './services/pure-components/button-switch/button-switch.component';
 import { AuthPlainService } from './services/authentication/auth.plain';
 import { GenericInterceptors } from './services/interceptors';
 import { CoreConfigService } from './services/core-config.service';
 import { ICoreConfig } from './shared/interfaces/config.interface';
-/*import {AccordionComponent} from './ionic-components/accordion/accordion/accordion.component';
-import {RestorePasswordComponent} from './ionic-components/restore-password/restore-password.component';
-import {ChangePasswordComponent} from './ionic-components/change-password/change-password.component';*/
 
 
 @NgModule({
@@ -31,12 +24,6 @@ import {ChangePasswordComponent} from './ionic-components/change-password/change
         IonicStorageModule.forRoot()
     ],
     declarations: [
-        //SignUpComponent,
-        //LoginComponent,
-        ButtonSwitchComponent,
-        //AccordionComponent,
-        //RestorePasswordComponent,
-        //ChangePasswordComponent,
     ],
     providers: [
         {
@@ -46,12 +33,6 @@ import {ChangePasswordComponent} from './ionic-components/change-password/change
         }
     ],
     exports: [
-        //SignUpComponent,
-        //LoginComponent,
-        ButtonSwitchComponent,
-        //AccordionComponent,
-        //RestorePasswordComponent,
-        //ChangePasswordComponent,
     ]
 })
 export class AngularCoreModule {
