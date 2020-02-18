f68cd32859438ebe0313fee4505613910885ff72
Riccardo Coco <coco@next.sa.it>
2020-02-18 18:17:58 +0100

    refact(Lib) btn switch deleted

--- src/public-api.ts
+++ src/public-api.ts
@@ -6,16 +6,8 @@
 
 export { AngularCoreModule } from './lib/angular-core.module';
 
-// COMPONENTS - ANGULAR
-export { ButtonSwitchComponent } from './lib/services/pure-components/button-switch/button-switch.component';
+// COMPONENTS
 export { SamePasswordValidator } from './lib/directives/same-password/same-password.directive';
-
-// COMPONENTS - IONIC
-/*export { AccordionComponent } from './lib/ionic-components/accordion/accordion/accordion.component';
-export { ChangePasswordComponent } from './lib/ionic-components/change-password/change-password.component';
-export { LoginComponent } from './lib/ionic-components/login/login.component';
-export { RestorePasswordComponent } from './lib/ionic-components/restore-password/restore-password.component';
-export { SignUpComponent } from './lib/ionic-components/sign-up/sign-up.component';*/
 
 // AUTH
 export { AuthWordpressService } from './lib/services/authentication/auth.wordpress';
