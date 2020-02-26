78ed53fe36447dbbe2231c24c44702d88190cae3
Riccardo Coco <coco@next.sa.it>
2020-02-26 12:49:50 +0100

    fix(App) logic moved to ng-afdd

--- schematics/ng-add/index.ts
+++ schematics/ng-add/index.ts
@@ -1,10 +1,36 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import {
+  Rule, Tree/*, SchematicContext*/
+} from '@angular-devkit/schematics';
+import { addModuleImportToRootModule, getProjectFromWorkspace } from 'schematics-utilities';
+import { getWorkspace } from '@schematics/angular/utility/config';
+import { ISchema as MyServiceSchema } from './schema';
 
-// Just return the tree
-export function ngAdd(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    _context.addTask(new NodePackageInstallTask());
-    return tree;
+export function myService(options: MyServiceSchema): Rule {
+  return (host: Tree/*, context: SchematicContext*/) => {
+      // get the workspace config of the consuming project
+      // i.e. angular.json file
+      const workspace = getWorkspace(host);
+
+      // identify the project config which is using our library
+      // or default to the default project in consumer workspace
+      const project = getProjectFromWorkspace(
+      workspace,
+      options.project || workspace.defaultProject
+      );
+
+      // inject our module into the current main module of the selected project
+      addModuleImportToRootModule(
+      // tree to modify
+      host,
+      // Module name to insert
+      'AngularCoreModule',
+      // project name for import statement
+      '@next-adv/angular-core',
+      // project to be modified
+      project
+      );
+
+      // return updated tree
+      return host;
   };
 }
