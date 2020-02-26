7f8db3a34dc89dfb1e11876e832d3f35f5df1f36
Riccardo Coco <coco@next.sa.it>
2020-02-26 11:52:20 +0100

    feat(Lib) auto module import

---
+++ schematics/ng-service/index.ts
@@ -0,0 +1,36 @@
+import {
+    Rule, Tree/*, SchematicContext*/
+} from '@angular-devkit/schematics';
+import { addModuleImportToRootModule, getProjectFromWorkspace } from 'schematics-utilities';
+import { getWorkspace } from '@schematics/angular/utility/config';
+import { ISchema as MyServiceSchema } from './schema';
+
+export function myService(options: MyServiceSchema): Rule {
+    return (host: Tree/*, context: SchematicContext*/) => {
+        // get the workspace config of the consuming project
+        // i.e. angular.json file
+        const workspace = getWorkspace(host);
+
+        // identify the project config which is using our library
+        // or default to the default project in consumer workspace
+        const project = getProjectFromWorkspace(
+        workspace,
+        options.project || workspace.defaultProject
+        );
+
+        // inject our module into the current main module of the selected project
+        addModuleImportToRootModule(
+        // tree to modify
+        host,
+        // Module name to insert
+        'AngularCoreModule',
+        // project name for import statement
+        '@next-adv/angular-core',
+        // project to be modified
+        project
+        );
+
+        // return updated tree
+        return host;
+    };
+}
