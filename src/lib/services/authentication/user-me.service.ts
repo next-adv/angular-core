535a6a08da0657f629bf0421685b41ab0d8aea0e
Riccardo Coco <coco@next.sa.it>
2020-03-10 09:45:48 +0100

    feat(Lib) user-me service impl

---
+++ src/lib/services/authentication/user-me.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CoreConfigService } from '../core-config.service';
+import { ICoreConfig } from '../../shared/interfaces/config.interface';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserMeService {
+
+    constructor(
+        private httpClient: HttpClient,
+        @Inject(CoreConfigService) private config: ICoreConfig,
+    ) {}
+
+    public getUserMe(token?: string, body?: any, options?: any): Promise<any> {
+        const userMePath = this.config.restApi
+        .restPathList.filter(value => value.prefix === 'main-api')
+        .find(value => value.type === 'userMe');
+        const opt = {
+            headers: {
+                authorization: 'Bearer ' + token
+            },
+            ...options
+        };
+
+        return this.httpClient.post(`/${userMePath.prefix}/${userMePath.url}`, body, opt).toPromise();
+    }
+}
--- src/public-api.ts
+++ src/public-api.ts
@@ -9,6 +9,7 @@ export { AngularCoreModule } from './lib/angular-core.module';
 // AUTH
 export { AuthWordpressService } from './lib/services/authentication/auth.wordpress';
 export { AuthPlainService } from './lib/services/authentication/auth.plain';
+export { UserMeService } from './lib/services/authentication/user-me.service';
 
 // HELPER
 export { UIHelperService } from './lib/services/helpers/ui-helper.service';
